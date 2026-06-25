26a6b7ccd29931286960f98166f126a8f6033a3f
Aniket Gudgal <114157042+Aniketgudgal@users.noreply.github.com>
2026-06-25 21:56:08 +0530

    Update TestController.java

--- SpringMVCApplication/src/main/java/com/spring/Controller/TestController.java
+++ SpringMVCApplication/src/main/java/com/spring/Controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class TestController {
 	@GetMapping("/")
 	public String getHome()
